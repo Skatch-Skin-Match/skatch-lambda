2e55210a3c6884d356e3849dbfdb79cf3b25b3a0
SarthakSS10 <sarthak.shah@vshsolutions.com>
2023-03-31 17:39:41 +0530

    added changes according to ecr

--- lambda_function.py
+++ lambda_function.py
@@ -1,23 +1,49 @@
 import requests
 import cv2
 import numpy as np
+import json
+# import colorsys
+
 def lambda_handler(event, context):  
     print("testtttt")
+    print('event',event)
+    if event:
+        url = event['url']
+
+    else :
+       url = 'https://d30ukgyabounne.cloudfront.net/face.jpeg'
+
+
+    print('event',url)
+
+
+    # body = json.loads(event['body'])
+    print("Hello AWS!")
+    # print("event = {}".format(event))
+    # return {
+    #     'statusCode': 200,
+    # }
     # val = userSkinTone.test()
-    val = userSkinTone()
+    val = userSkinTone(url)
     print('vvvvvvvvvvvv',val)
-    return val
+    # return val
+    return {
+    'statusCode': 200,
+    'headers': {'Content-Type': 'application/json'},
+    'body': val
+}
     # response = requests.get("https://www.example.com/")
     # print(response.text)
     # return response.text
-
 def avgHSVCalc(imgHSV):
+    print('inside avgHSVCalc')
+
     imgHue = 0
     imgSat = 0
     imgVib = 0
 
     pixelCount = len(imgHSV[0]) * len(imgHSV) # col * row
-
+    print('pixelCount',pixelCount)
 
     for i in imgHSV: # i is a particular row
         for n in i: # n is a partcular column
@@ -42,18 +68,25 @@ def avgHSVCalc(imgHSV):
     # avgImgSat = (avgImgSat / 255) * 100
     # avgImgVib = (avgImgVib / 255) * 100
     # /\ If commented out, we are using 0-255 range for S&V.
-
+    print('3')
     return [avgImgHue, avgImgSat, avgImgVib]
 
 
-def userSkinTone():
+def userSkinTone(url):
+    print('inside ust')
     # # Load the image
     # userImg = cv2.imread('https://lp2.hm.com/hmgoepprod?set=quality%5B79%5D%2Csource%5B%2Fe8%2F4e%2Fe84e3d6cf5155ac862bafa692f10346c3957fa96.jpg%5D%2Corigin%5Bdam%5D%2Ccategory%5B%5D%2Ctype%5BLOOKBOOK%5D%2Cres%5Bm%5D%2Chmver%5B1%5D&call=url[file:/product/fullscreen]')
 
-    url = 'https://lp2.hm.com/hmgoepprod?set=quality%5B79%5D%2Csource%5B%2Fe8%2F4e%2Fe84e3d6cf5155ac862bafa692f10346c3957fa96.jpg%5D%2Corigin%5Bdam%5D%2Ccategory%5B%5D%2Ctype%5BLOOKBOOK%5D%2Cres%5Bm%5D%2Chmver%5B1%5D&call=url[file:/product/fullscreen]'
+    # url = 'https://lp2.hm.com/hmgoepprod?set=quality%5B79%5D%2Csource%5B%2Fe8%2F4e%2Fe84e3d6cf5155ac862bafa692f10346c3957fa96.jpg%5D%2Corigin%5Bdam%5D%2Ccategory%5B%5D%2Ctype%5BLOOKBOOK%5D%2Cres%5Bm%5D%2Chmver%5B1%5D&call=url[file:/product/fullscreen]'
+
+    # url = 'https://lp2.hm.com/hmgoepprod?set=quality%5B79%5D%2Csource%5B%2F44%2Fc2%2F44c2b04b3b6019b91a58db1b5ee07b0d54503432.jpg%5D%2Corigin%5Bdam%5D%2Ccategory%5B%5D%2Ctype%5BLOOKBOOK%5D%2Cres%5Bm%5D%2Chmver%5B1%5D&call=url[file:/product/main]'
+    # url = 'https://d30ukgyabounne.cloudfront.net/face.jpeg'
+
     response = requests.get(url)
     img_array = np.asarray(bytearray(response.content), dtype=np.uint8)
     userImg = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
+    print('1')
+
 
     # Convert BGR to HSV
     userHSV = cv2.cvtColor(userImg, cv2.COLOR_BGR2HSV)
@@ -68,10 +101,24 @@ def userSkinTone():
     # Apply the mask to the original image to extract the skin tone area
     userSkin = cv2.bitwise_and(userImg, userImg, mask=userMask)
     userSkinHSV = cv2.cvtColor(userSkin, cv2.COLOR_BGR2HSV)
-
+    print('2')
 
 
     userAvgHSV = avgHSVCalc(userSkinHSV)
+    print('4')
+
+
+#     print(f"User Skin Avg HSV: {userAvgHSV[0]}")
+
+#     rgb_color = colorsys.hsv_to_rgb(userAvgHSV[0]/360, userAvgHSV[1]/255, userAvgHSV[2]/255)
+
+# # Convert RGB to HEX
+#     hex_color = '#{:02x}{:02x}{:02x}'.format(int(rgb_color[0]*255), int(rgb_color[1]*255), int(rgb_color[2]*255))
+
+#     print("HSV color: ", [10.085380241738104, 105.99668645588845, 159.1227768503759])
+#     print("HEX color: ", hex_color)
+    
     print(f"User Skin Avg HSV: {userAvgHSV}")
     return userAvgHSV
 
+# lambda_handler('','')
